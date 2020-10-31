e255a0fbacccf9dc20eca6cf68ed31ac965994c2
RanZhang-CR <59900405+RanZhang-CR@users.noreply.github.com>
2020-10-31 12:02:08 -0400

    initial implementation

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 #include "immintrin.h"
 
 #include "kernel.h"
@@ -11,93 +12,101 @@
 int main(){
   //data sets    
   //[x1,x2,x3,x4...x28,y1,y2,y3,y4....y28, ..... , final_dimension1,final_dimension2,...final_dimension28]
-  double *a;   
+  double *x;   
 
   //test points
   //[a1,b1,c1, ..., final_dimension1, a2, b2,c2, ... final_dimension2, ..., ]    
-  double *b;      
+  double *a;      
 
-  double *l;      //labels
+  bool *l;      // training labels for x
 
-  // distances without squre root
+  bool *test_l  // finding labels for test point      
+
+
+  // r[i] is the distance between one training point ans one test point
   double *r;
 
   //double *c, *c_check;
+  
+  int starting_dimension = 2;  //  benchmark performance for 2D
+  int ending_dimension  =  5;  //  benchmark performance for 5D
+
+  int RUNS = 100;    // can be changed
 
 
-  // TODO on OCT 31st
-  unsigned long long t0, t1, sum;
+  int kernel_size  = 28 ;
 
-  //CHANGE THESE
-  int mc = 96;  //mc is the number of rows of A
-  int kc = 256;  //kc is the number of columns of A
 
-  //DO NOT CHANGE THESE
-  int m  = 6;  //m is the number of rows of the kernel
-  int n  = 8;  //n is the number of columns of the kernel
+  int x_size = kernel_size * 10;     //number of data set points (arbitrary, can be changed)
+  int a_size = 20;                   //number of test points (arbitrary, can be changed)
+  
+  int x_jump_size  = 28;  //x_jump_size is the number need to be jumped each time when calling kernel
 
-  /*
-    Assume the following
-        - A is stored in column major order
-        - B is stored in row major order
-        - C must be written out in row major order
-  */
+ 
 
-  for (int k = kc/2; k <= 2*kc; k *= 2){
+  for (int k = starting_dimension ; k <= ending_dimension ;k++){
 
-    posix_memalign((void**) &a, 64, mc * k * sizeof(double));
-    posix_memalign((void**) &b, 64, n * k * sizeof(double));
-    posix_memalign((void**) &c, 64, mc * n * sizeof(double));
-    posix_memalign((void**) &c_check, 64, mc * n * sizeof(double));
+    posix_memalign((void**) &x, 64, x_size * k * sizeof(double));
+    posix_memalign((void**) &a, 64, a_size * k * sizeof(double));
+    posix_memalign((void**) &r, 64, a_size * x_size * sizeof(double));
+    posix_memalign((void**) &l, 64, x_size * sizeof(bool));
+    posix_memalign((void**) &test_l, 64, a_size * sizeof(bool));
+    
 
-    //initialize A
-    for (int i = 0; i != k * mc; ++i){
+    //initialize data set 
+    for (int i = 0; i != x_size * k; ++i){
+      x[i] = ((double) rand())/ ((double) RAND_MAX);
+    }
+    //initialize test points
+    for (int i = 0; i != a_size * k; ++i){
       a[i] = ((double) rand())/ ((double) RAND_MAX);
     }
-    //initialize B
-    for (int i = 0; i != k * n; ++i){
-      b[i] = ((double) rand())/ ((double) RAND_MAX);
-    }
-    //initialize C
-    for (int i = 0; i != mc * n; ++i){
-      c[i] = 0.0;
-      c_check[i] = 0.0;
+
+    //initialize r 
+    for (int i = 0; i != a_size * x_size; ++i){
+      r[i] = ((double) rand())/ ((double) RAND_MAX);
     }
 
-    printf("%d\t %d\t %d\t", mc, n, k);
 
-    sum = 0;
+    //initialize l
+    for (int i = 0; i != x_size; ++i){
+      double temp  = ((double) rand())/ ((double) RAND_MAX);
+     if ( temp < 0.5)
+      l[i] = 0;
+    else
+      l[i] = 1; 
+    }
+    }    
+    //initialize test_l 
+    for (int i = 0; i != a_size; ++i){
+      test_l[i] = 0;
+    }
+
+
+    // KNN implementation
+    sum = 0;       // count cycles consuming
     for (int runs = 0; runs != RUNS; ++runs){
       t0 = rdtsc();
-      for (int i = 0; i != mc / m; ++i){	
-	naive(6, 8, k, a + i*m*k, b, c_check + i*6*8);      
+      itt = 0;
+      for (int j = 0; j != a_size*k; j+=k){
+        for (int i = 0; i != x_size*k; i+=kernel_size*k){	
+            kernel(x+i, a+j, r + itt*kernel_size, k);
+            itt++;    
+        }
       }
       t1 = rdtsc();
       sum += (t1 - t0);
     }
-    printf(" %lf\t", (2.0*mc*n*k)/(sum/(1.0*RUNS)));  
-
-
-
-    sum = 0;
-    for (int runs = 0; runs != RUNS; ++runs){
-      t0 = rdtsc();
-      for (int i = 0; i != mc / m; ++i){	
-	kernel(k, a + i*m*k, b, c + i*6*8);      
-      }
-      t1 = rdtsc();
-      sum += (t1 - t0);
-    }
-
+    /*
     int correct = 1;
     for (int i = 0; i != m * n; ++i)
       correct &= (fabs(c[i] - c_check[i]) < 1e-12);
     printf(" %lf\t%d\n", (2.0*mc*n*k)/((double)(sum/(1.0*RUNS))), correct);
-
+    */
+    free(x);
     free(a);
-    free(b);
-    free(c);
-    free(c_check);
+    free(r);
+    free(l);
   }
 
   
