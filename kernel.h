c567608b6667f7786e52548e1d705f0a1e31f0ca
RanZhang-CR <59900405+RanZhang-CR@users.noreply.github.com>
2020-11-05 17:14:51 -0500

    new

--- kernel.h
+++ kernel.h
@@ -18,15 +18,38 @@ void inline kernel(
 
     for(int i=0; i < D; ++i){
         // load data
-        __m256d smd1 = _mm256_loadu_pd(x + i*28);
-        __m256d smd2 = _mm256_loadu_pd(x+4+ i*28);
-        __m256d smd3 = _mm256_loadu_pd(x+8+ i*28);
-        __m256d smd4 = _mm256_loadu_pd(x+12+ i*28);
-        __m256d smd5 = _mm256_loadu_pd(x+16+ i*28);
-        __m256d smd6 = _mm256_loadu_pd(x+20+ i*28);
-        __m256d smd7 = _mm256_loadu_pd(x+24+ i*28);
+		__m256d smd0 = _mm256_broadcast_sd(a + i);
 
-        __m256d smd0 = _mm256_broadcast_sd(a + i);
+
+        __m256d smd1 = _mm256_loadu_pd(x + i*28);
+		smd1 = _mm256_sub_pd(smd0, smd1);
+		smd8 = _mm256_fmadd_pd(smd1, smd1, smd8);
+
+        __m256d smd2 = _mm256_loadu_pd(x+4+ i*28);
+		smd2 = _mm256_sub_pd(smd0, smd2);
+		smd9 = _mm256_fmadd_pd(smd2, smd2, smd9);
+
+        __m256d smd3 = _mm256_loadu_pd(x+8+ i*28);
+		smd3 = _mm256_sub_pd(smd0, smd3);
+		smd10 = _mm256_fmadd_pd(smd3, smd3, smd10);
+
+        __m256d smd4 = _mm256_loadu_pd(x+12+ i*28);
+		smd4 = _mm256_sub_pd(smd0, smd4);
+		smd11 = _mm256_fmadd_pd(smd4, smd4, smd11);
+
+        __m256d smd5 = _mm256_loadu_pd(x+16+ i*28);
+		smd5 = _mm256_sub_pd(smd0, smd5);
+		smd12 = _mm256_fmadd_pd(smd5, smd5, smd12);
+
+        __m256d smd6 = _mm256_loadu_pd(x+20+ i*28);
+		smd6 = _mm256_sub_pd(smd0, smd6);
+		smd13 = _mm256_fmadd_pd(smd6, smd6, smd13);
+
+        __m256d smd7 = _mm256_loadu_pd(x+24+ i*28);
+		smd7 = _mm256_sub_pd(smd0, smd7);
+		smd14 = _mm256_fmadd_pd(smd7, smd7, smd14);
+
+        
 
         // __m256d smd8 = _mm256_loadu_pd(r);
         // __m256d smd9 = _mm256_loadu_pd(r+4);
@@ -38,22 +61,21 @@ void inline kernel(
 
 
         // subtraction part
-        smd1 = _mm256_sub_pd(smd0, smd1);
-        smd2 = _mm256_sub_pd(smd0, smd2);
-        smd3 = _mm256_sub_pd(smd0, smd3);
-        smd4 = _mm256_sub_pd(smd0, smd4);
-        smd5 = _mm256_sub_pd(smd0, smd5);
-        smd6 = _mm256_sub_pd(smd0, smd6);
-        smd7 = _mm256_sub_pd(smd0, smd7);
+        
+        
+        
+        
+        
+        
 
         // fma part
-        smd8 = _mm256_fmadd_pd(smd1, smd1, smd8);
-        smd9 = _mm256_fmadd_pd(smd2, smd2, smd9);
-        smd10 = _mm256_fmadd_pd(smd3, smd3, smd10);
-        smd11 = _mm256_fmadd_pd(smd4, smd4, smd11);
-        smd12 = _mm256_fmadd_pd(smd5, smd5, smd12);
-        smd13 = _mm256_fmadd_pd(smd6, smd6, smd13);
-        smd14 = _mm256_fmadd_pd(smd7, smd7, smd14);
+        
+        
+        
+        
+        
+        
+        
     }
 
     //_mm256_extract_ps  to pull out the data
