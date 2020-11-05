ec50fe54720d7c4228c36807b73f1ca268f7e378
RanZhang-CR <59900405+RanZhang-CR@users.noreply.github.com>
2020-11-05 16:14:26 -0500

    new

--- main.c
+++ main.c
@@ -34,14 +34,14 @@ int main(){
 
   //double *c, *c_check;
   
-  int starting_dimension = 2;  //  benchmark performance for 2D
-  int ending_dimension  =  5;  //  benchmark performance for 5D
+  int starting_dimension = 1;  //  benchmark performance for 2D
+  int ending_dimension  =  10;  //  benchmark performance for 5D
 
   int kernel_size  = 28 ;
 
 
-  int x_size = kernel_size * 1;     //number of data set points (arbitrary, can be changed)
-  int a_size = 1;                   //number of test points (arbitrary, can be changed)
+  int x_size = kernel_size * 20;     //number of data set points (arbitrary, can be changed)
+  int a_size = 20;                   //number of test points (arbitrary, can be changed)
   
   int x_jump_size  = 28;  //x_jump_size is the number need to be jumped each time when calling kernel
 
@@ -59,24 +59,24 @@ int main(){
     //initialize data set 
     for (int i = 0; i != x_size * k; ++i){
       x[i] = ((double) rand())/ ((double) RAND_MAX);
-      printf("%lf,",x[i]);
+      //printf("%lf,",x[i]);
       if(i==x_size-1){
-        printf("y:\n");
+        //printf("y:\n");
       }
     }
-    printf("\n a: \n");
+    //printf("\n a: \n");
     //initialize test points
     for (int i = 0; i != a_size * k; ++i){
       a[i] = ((double) rand())/ ((double) RAND_MAX);
-      printf("%lf\t",a[i]);
+      //printf("%lf\t",a[i]);
     }
-    printf("\n");
+   // printf("\n");
     //initialize r 
     for (int i = 0; i != a_size * x_size; ++i){
       r[i] = 0;//((double) rand())/ ((double) RAND_MAX);
     }
 
-    printf("l : \n");
+   // printf("l : \n");
     //initialize l
     for (int i = 0; i != x_size; ++i){
       double temp  = ((double) rand())/ ((double) RAND_MAX);
@@ -84,9 +84,9 @@ int main(){
       l[i] = 0;
     else
       l[i] = 1; 
-      printf("%d\t",l[i]);
+     // printf("%d\t",l[i]);
     }
-    printf("\n");
+   // printf("\n");
   
     //initialize test_l 
     for (int i = 0; i != a_size; ++i){
@@ -101,7 +101,7 @@ int main(){
       int itt = 0;
       for (int j = 0; j != a_size*k; j+=k){
         for (int i = 0; i != x_size*k; i+=kernel_size*k){	
-            printf("i=%d j=%d itt=%d k=%d \n",i,j,itt*kernel_size,k);
+            //printf("i=%d j=%d itt=%d k=%d \n",i,j,itt*kernel_size,k);
             kernel(x+i, a+j, r + itt*kernel_size, k);
             itt++;    
         }
@@ -120,12 +120,11 @@ int main(){
     int correct = 1;
     // for (int i = 0; i != m * n; ++i)
     //   correct &= (fabs(c[i] - c_check[i]) < 1e-12);
-    printf(" %lf\t%d\n", (a_size*x_size*5*k)/((double)(sum/(1.0*RUNS))), correct);
+    printf("cycles consuming %lf\t%d, dimenssion: %d\n", (a_size*x_size*5*k)/((double)(sum/(1.0*RUNS))), correct, k);
     
     free(x);
     free(a);
     free(r);
-    break;
   }
 
   
