bdd8be18aeaef73682c72cedac0bed1342152d18
RanZhang-CR <59900405+RanZhang-CR@users.noreply.github.com>
2020-12-08 13:21:14 -0500

    generate list map

--- generate_anotations.py
+++ generate_anotations.py
@@ -19,11 +19,13 @@ with open('FLIR_ADAS_1_3/train/thermal_annotations.json', mode='r', encoding='gb
                     tmp += str(float(b['bbox'][j]/640)) + " "
             file_content_list[id] += tmp
 
+
+train_list_map = []
 for n in range(len(file_list)):
     if file_content_list[n] != "":
         with open("data/custom/labels/"+file_list[n],mode='w',encoding='gbk') as w2:
             w2.write(file_content_list[n])
-            print("data/custom/images/"+file_list[n][:-3] +"jpeg")
+            train_list_map.append("data/custom/images/"+file_list[n][:-3] +"jpg")
 
 
 file_list = []
@@ -47,9 +49,18 @@ with open('FLIR_ADAS_1_3/val/thermal_annotations.json', mode='r', encoding='gbk'
             file_content_list[id] += tmp
 
 print("\n\n")
-
+valid_list_map = []
 for n in range(len(file_list)):
     if file_content_list[n] != "":
         with open("data/custom/labels/"+file_list[n],mode='w',encoding='gbk') as w2:
             w2.write(file_content_list[n])
-            print("data/custom/images/"+file_list[n][:-3] +"jpeg")
+            valid_list_map.append("data/custom/images/"+file_list[n][:-3] +"jpg")
+
+
+with open("data/custom/train.txt",mode='w',encoding='gbk') as f:
+    for i in train_list_map:
+        f.write(i + "\n")
+
+with open("data/custom/valid.txt",mode='w',encoding='gbk') as f:
+    for i in valid_list_map:
+        f.write(i + "\n")
